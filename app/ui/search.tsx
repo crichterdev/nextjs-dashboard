79b9b48eec2e435ef3a0b1368d7020d532cd25ae
carlos-richter <rrch.cr8@gmail.com>
2024-10-03 00:21:29 -0400

    11.2 pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,16 +10,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-    const handleSearch = useDebouncedCallback((term) => {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1')
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-    }, 300);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -32,7 +33,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-       defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
