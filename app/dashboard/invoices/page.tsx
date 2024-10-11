a0946af0e36ed13fc10975258626952d9509e199
carlos-richter <rrch.cr8@gmail.com>
2024-10-11 00:05:14 -0400

    12.1 Mutating data - create an invoice

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,13 +8,11 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function Page({
-    searchParams,
+    searchParams: rawSearchParams,
 }: {
-    searchParams?: {
-        query?: string;
-        page?: string;
-    };
+    searchParams?: Promise<{ query?: string; page?: string }>;
 }) {
+    const searchParams = await rawSearchParams;
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
     const totalPages = await fetchInvoicesPages(query);
